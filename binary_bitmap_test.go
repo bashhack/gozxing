394b44c8787ea292a1cb9ba88fa665a450340763
Marc Alvarez <marcalvarez@fastmail.com>
2025-07-17 09:43:51 -0600

    Add test coverage for thread-safe implementation

--- binary_bitmap_test.go
+++ binary_bitmap_test.go
@@ -1,6 +1,11 @@
 package gozxing
 
 import (
+	"image"
+	"image/color"
+	"runtime"
+	"sync"
+	"sync/atomic"
 	"testing"
 
 	errors "golang.org/x/xerrors"
@@ -198,4 +203,230 @@ func TestBinaryBitmap_Illegal(t *testing.T) {
 	if s := bmp.String(); s != expect {
 		t.Fatalf("string = \"%v\", expect \"%v\"", s, expect)
 	}
+}
+
+func TestBinaryBitmap_SafeConcurrent(t *testing.T) {
+	img := image.NewGray(image.Rect(0, 0, 200, 200))
+	for y := 0; y < 200; y++ {
+		for x := 0; x < 200; x++ {
+			img.Set(x, y, color.Gray{uint8((x + y) % 256)})
+		}
+	}
+
+	bmp, err := NewBinaryBitmapFromImage(img)
+	if err != nil {
+		t.Fatalf("Failed to create binary bitmap: %v", err)
+	}
+
+	var wg sync.WaitGroup
+	var panicCount int32
+	var successCount int32
+
+	numGoroutines := runtime.NumCPU() * 100
+
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+
+			defer func() {
+				if r := recover(); r != nil {
+					atomic.AddInt32(&panicCount, 1)
+					t.Errorf("Goroutine %d panicked: %v", id, r)
+				}
+			}()
+
+			// Prior to introduction of sync.Once,
+			// this call triggered the race condition
+			matrix, err := bmp.GetBlackMatrix()
+			if err != nil {
+				t.Errorf("Goroutine %d got error: %v", id, err)
+				return
+			}
+
+			// Try to access the matrix - before thread-safe changes, would panic
+			for y := 0; y < matrix.GetHeight(); y++ {
+				for x := 0; x < matrix.GetWidth(); x++ {
+					_ = matrix.Get(x, y)
+				}
+			}
+
+			atomic.AddInt32(&successCount, 1)
+		}(i)
+	}
+
+	wg.Wait()
+
+	if panicCount > 0 {
+		t.Errorf("Got %d panics accessing shared BinaryBitmap", panicCount)
+	}
+
+	t.Logf("All %d goroutines completed successfully (0 panics)", successCount)
+}
+
+func TestHybridBinarizer_SafeConcurrent(t *testing.T) {
+	img := image.NewGray(image.Rect(0, 0, 200, 200))
+	for y := 0; y < 200; y++ {
+		for x := 0; x < 200; x++ {
+			img.Set(x, y, color.Gray{uint8((x + y) % 256)})
+		}
+	}
+
+	source := NewLuminanceSourceFromImage(img)
+	binarizer := NewHybridBinarizer(source)
+
+	var wg sync.WaitGroup
+	var panicCount int32
+	var successCount int32
+
+	numGoroutines := runtime.NumCPU() * 100
+
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+
+			defer func() {
+				if r := recover(); r != nil {
+					atomic.AddInt32(&panicCount, 1)
+					t.Errorf("Goroutine %d panicked: %v", id, r)
+				}
+			}()
+
+			matrix, err := binarizer.GetBlackMatrix()
+			if err != nil {
+				t.Errorf("Goroutine %d got error: %v", id, err)
+				return
+			}
+
+			for y := 0; y < matrix.GetHeight(); y++ {
+				for x := 0; x < matrix.GetWidth(); x++ {
+					_ = matrix.Get(x, y)
+				}
+			}
+
+			atomic.AddInt32(&successCount, 1)
+		}(i)
+	}
+
+	wg.Wait()
+
+	if panicCount > 0 {
+		t.Errorf("Got %d panics accessing shared HybridBinarizer", panicCount)
+	}
+
+	t.Logf("All %d goroutines completed successfully with HybridBinarizer (0 panics)", successCount)
+}
+
+func TestGetBlackMatrixCalledOnce(t *testing.T) {
+	img := image.NewGray(image.Rect(0, 0, 100, 100))
+
+	t.Run("BinaryBitmap", func(t *testing.T) {
+		source := NewLuminanceSourceFromImage(img)
+		callCount := int32(0)
+
+		countingBinarizer := &countingBinarizer{
+			Binarizer: NewHybridBinarizer(source),
+			count:     &callCount,
+		}
+
+		bmp, _ := NewBinaryBitmap(countingBinarizer)
+
+		var wg sync.WaitGroup
+		for i := 0; i < 100; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				_, _ = bmp.GetBlackMatrix()
+			}()
+		}
+		wg.Wait()
+
+		if count := atomic.LoadInt32(&callCount); count != 1 {
+			t.Errorf("GetBlackMatrix was called %d times, expected 1", count)
+		}
+	})
+
+	t.Run("HybridBinarizer", func(t *testing.T) {
+		source := NewLuminanceSourceFromImage(img)
+		binarizer := NewHybridBinarizer(source)
+
+		var wg sync.WaitGroup
+		matrices := make([]*BitMatrix, 100)
+
+		for i := 0; i < 100; i++ {
+			wg.Add(1)
+			go func(idx int) {
+				defer wg.Done()
+				matrix, _ := binarizer.GetBlackMatrix()
+				matrices[idx] = matrix
+			}(i)
+		}
+		wg.Wait()
+
+		firstMatrix := matrices[0]
+		for i := 1; i < 100; i++ {
+			if matrices[i] != firstMatrix {
+				t.Errorf("Matrix %d is a different instance than matrix 0", i)
+			}
+		}
+	})
+}
+
+// countingBinarizer counts how many times GetBlackMatrix is called
+type countingBinarizer struct {
+	Binarizer
+	count *int32
+}
+
+func (c *countingBinarizer) GetBlackMatrix() (*BitMatrix, error) {
+	atomic.AddInt32(c.count, 1)
+	return c.Binarizer.GetBlackMatrix()
+}
+
+// TestRaceDetector illustrates thread-safe implementation leveraging sync.Once
+// Run with -race to observe behavior and outcomes
+func TestRaceDetector(t *testing.T) {
+	img := image.NewGray(image.Rect(0, 0, 100, 100))
+	bmp, _ := NewBinaryBitmapFromImage(img)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, _ = bmp.GetBlackMatrix()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkConcurrentGetBlackMatrix(b *testing.B) {
+	img := image.NewGray(image.Rect(0, 0, 200, 200))
+	for y := 0; y < 200; y++ {
+		for x := 0; x < 200; x++ {
+			img.Set(x, y, color.Gray{uint8((x + y) % 256)})
+		}
+	}
+
+	b.Run("BinaryBitmap", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			bmp, _ := NewBinaryBitmapFromImage(img)
+			for pb.Next() {
+				_, _ = bmp.GetBlackMatrix()
+			}
+		})
+	})
+
+	b.Run("HybridBinarizer", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			source := NewLuminanceSourceFromImage(img)
+			binarizer := NewHybridBinarizer(source)
+			for pb.Next() {
+				_, _ = binarizer.GetBlackMatrix()
+			}
+		})
+	})
 }
